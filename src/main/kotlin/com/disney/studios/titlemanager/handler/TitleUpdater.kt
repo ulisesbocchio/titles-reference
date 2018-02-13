d9b356abba5660579f3e4e72ac5a044920677eb0
Ulises Bocchio <ubocchio@ULINEWPRO.local>
2018-02-12 23:32:51 -0800

    first tests

--- src/main/kotlin/com/disney/studios/titlemanager/handler/TitleUpdater.kt
+++ src/main/kotlin/com/disney/studios/titlemanager/handler/TitleUpdater.kt
@@ -19,54 +19,56 @@ class TitleUpdater(val update: Mono<Title>) : TitleVisitor<Mono<Title>> {
     override fun visit(title: ChildTitle): Mono<Title> {
         return visit(title as Title)
                 .cast<ChildTitle>()
-                .doOnError { throw HttpClientErrorException(HttpStatus.BAD_REQUEST) }
                 .map {
                     title.parent = it.parent ?: title.parent
-                    it
+                    it as Title
                 }
+                .onErrorMap { badRequest() }
     }
 
     override fun visit(title: Bonus): Mono<Title> {
         return visit(title as ChildTitle)
                 .cast<Bonus>()
-                .doOnError { throw HttpClientErrorException(HttpStatus.BAD_REQUEST) }
                 .map {
                     title.duration = it.duration ?: title.duration
-                    title
+                    title as Title
                 }
+                .onErrorMap { badRequest() }
     }
 
     override fun visit(title: TvSeries): Mono<Title> {
         return visit(title as Title)
                 .cast<TvSeries>()
-                .doOnError { throw HttpClientErrorException(HttpStatus.BAD_REQUEST) }
                 .map {
                     title.releaseDate = it.releaseDate ?: title.releaseDate
                     title.seasons = it.seasons ?: title.seasons
-                    title
+                    title as Title
                 }
+                .onErrorMap { badRequest() }
     }
 
     override fun visit(title: Season): Mono<Title> {
         return visit(title as ChildTitle)
                 .cast<Season>()
-                .doOnError { throw HttpClientErrorException(HttpStatus.BAD_REQUEST) }
                 .map {
                     title.releaseDate = it.releaseDate ?: title.releaseDate
                     title.episodes = it.episodes ?: title.episodes
-                    title
+                    title as Title
                 }
+                .onErrorMap { badRequest() }
     }
 
     override fun visit(title: Episode): Mono<Title> {
         return visit(title as ChildTitle)
                 .cast<Episode>()
-                .doOnError { throw HttpClientErrorException(HttpStatus.BAD_REQUEST) }
                 .map {
                     title.releaseDate = it.releaseDate ?: title.releaseDate
                     title.duration = it.duration ?: title.duration
-                    title
+                    title as Title
                 }
+                .onErrorMap { badRequest() }
     }
+
+    private fun badRequest() = HttpClientErrorException(HttpStatus.BAD_REQUEST)
 
 }
--- src/test/kotlin/com/disney/studios/titlemanager/TitleManagerApplicationTests.kt
+++ src/test/kotlin/com/disney/studios/titlemanager/TitleManagerApplicationTests.kt
@@ -1,9 +1,9 @@
 package com.disney.studios.titlemanager
 
+import com.disney.studios.titlemanager.document.Title
 import com.disney.studios.titlemanager.repository.TitleRepository
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration
 import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerAutoConfiguration
@@ -12,36 +12,48 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.test.StepVerifier
 
-@RunWith(SpringRunner::class)
-@SpringBootTest(classes = [
+@SpringJUnitConfig(classes = [
     TitleManagerApplication::class,
     ReactiveWebServerAutoConfiguration::class,
     HttpHandlerAutoConfiguration::class,
-    WebFluxAutoConfiguration::class],
-        webEnvironment = RANDOM_PORT)
+    WebFluxAutoConfiguration::class])
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @DataMongoTest
 class TitleManagerApplicationTests {
 
     @LocalServerPort
     var port: Int? = null
 
-    lateinit var client: WebClient
+    lateinit var client: WebTestClient
 
     @Autowired
     lateinit var titlesRepo: TitleRepository
 
-    @Before
+    @BeforeEach
     fun setup() {
-        client = WebClient.create("http://localhost:$port")
+        client = WebTestClient.bindToServer().baseUrl("http://localhost:$port").build()
     }
 
     @Test
-	fun contextLoads() {
-        println("AAAAAAAAAAAA")
-	}
+    fun shouldFindAllLoadedTitlesFromRepository() {
+        StepVerifier.create(titlesRepo.findAll())
+                .expectNextCount(44)
+                .verifyComplete()
+    }
+
+    @Test
+    fun shouldFindAllLoadedTitlesFromApi() {
+        client.
+                get()
+                .uri("/titles")
+                .exchange()
+                .expectBodyList(Title::class.java)
+                .hasSize(44)
+    }
 
 }
 
